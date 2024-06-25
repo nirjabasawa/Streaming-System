20b3907d257d19aad0e0ab3a3c1798dcf841fbf2
Nirja Basawa <113481461+nirjabasawa@users.noreply.github.com>
2024-06-25 16:17:48 -0700

    add comment

--- StreamingSystemApp.java
+++ StreamingSystemApp.java
@@ -1,5 +1,5 @@
 package com.company;
-
+// start up streaming system
 public class StreamingSystemApp {
     public static void main(String[] args) throws Unplayable{
         StreamingSystem spotify = new StreamingSystem();
